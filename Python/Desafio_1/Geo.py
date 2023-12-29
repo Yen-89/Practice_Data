ca5d8de481142d166ecd09f18546c7a4fc3e316b
Yen <yenifer.amayaos@gmail.com>
2023-12-28 20:02:20 -0500

    prueba

--- Python/Desafio_1/Geo.py
+++ Python/Desafio_1/Geo.py
@@ -13,7 +13,7 @@ api_key = os.getenv("API_KEY")
 
 ciudad = input("Ingrese una ciudad: ")
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}"
+url = f"http://api.openweathermap.org/geo/1.0/direct?q={ciudad}&limit=5&appid={api_key}"
 
 res = requests.get(url)
 
@@ -21,16 +21,16 @@ data = res.json()
 
 #definir 3 caracteristicas ambientales: temperatura, velocidad del viento y descripción general meterolo, lat y lonh
 
-temp = data["main"]["temp"] #atributos main y temp porque en la data, temp es un diccionario que está dentro de otro diccionario que es main
-vel_viento = data["wind"]["speed"]
+#temp = data["main"]["temp"] #atributos main y temp porque en la data, temp es un diccionario que está dentro de otro diccionario que es main
+#vel_viento = data["wind"]["speed"]
 
-latitud = data["coord"]["lat"]
-longitud = data["coord"]["lon"]
+latitud = data["lat"]["lat"]
+longitud = data["lon"]["lon"]
 
-descripcion = data["weather"][0]["description"] #se pone el 0 ya que description es diferente, ya que es un diccionario dentro de una lista 
+#descripcion = data["weather"][0]["description"] #se pone el 0 ya que description es diferente, ya que es un diccionario dentro de una lista 
 
-print("la temperatura es: ", temp)
-print("la velocidad del viento es : ", vel_viento)
+#print("la temperatura es: ", temp)
+#print("la velocidad del viento es : ", vel_viento)
 print("la latitud es: ", latitud)
 print("la longitud es: ", longitud)
-print("la descripción es: ", descripcion)
+#print("la descripción es: ", descripcion)
