3da114e0f2054368195b79b0b52e28f93b502900
Yen <yenifer.amayaos@gmail.com>
2023-12-28 19:28:51 -0500

    Lat y Lon

---
+++ Python/Desafio_1/Geo.py
@@ -0,0 +1,36 @@
+import requests
+
+import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Clave de la API desde las variables de entorno
+api_key = os.getenv("API_KEY")
+
+# Usar la variable api_key para realizar la consulta a la API
+
+ciudad = input("Ingrese una ciudad: ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}"
+
+res = requests.get(url)
+
+data = res.json()
+
+#definir 3 caracteristicas ambientales: temperatura, velocidad del viento y descripción general meterolo, lat y lonh
+
+temp = data["main"]["temp"] #atributos main y temp porque en la data, temp es un diccionario que está dentro de otro diccionario que es main
+vel_viento = data["wind"]["speed"]
+
+latitud = data["coord"]["lat"]
+longitud = data["coord"]["lon"]
+
+descripcion = data["weather"][0]["description"] #se pone el 0 ya que description es diferente, ya que es un diccionario dentro de una lista 
+
+print("la temperatura es: ", temp)
+print("la velocidad del viento es : ", vel_viento)
+print("la latitud es: ", latitud)
+print("la longitud es: ", longitud)
+print("la descripción es: ", descripcion)
