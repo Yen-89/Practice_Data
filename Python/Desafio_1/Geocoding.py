03acf1501b5d48567cbf1292576a7c594a98ad3f
Yen <yenifer.amayaos@gmail.com>
2024-01-03 07:27:02 -0500

    Exam_1

--- Python/Desafio_1/Geocoding.py
+++ Python/Desafio_1/Geocoding.py
@@ -3,10 +3,12 @@ import requests
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "bcc938b34848481d2843119f11e2b331"
-CITY = "London"
+CITY = "Medellin"
 
 url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 
 response = requests.get(url).json()
 
-print(response)
+#print(response)
+
+
