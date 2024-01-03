c1d7f6d7e0dc2cf88cbd83785b74ad4088baff82
Yen <yenifer.amayaos@gmail.com>
2024-01-03 11:42:20 -0500

    geo

--- Python/Desafio_1/Geo.py
+++ Python/Desafio_1/Geo.py
@@ -1,14 +1,8 @@
+import datetime as dt
 import requests
 
-import os
-from dotenv import load_dotenv
-
-# Cargar variables de entorno desde el archivo .env
-load_dotenv()
-
 # Clave de la API desde las variables de entorno
-api_key = os.getenv("API_KEY")
-
+api_key = "bcc938b34848481d2843119f11e2b331"
 
 # Coordenadas de la ciudad (reemplazar con las coordenadas de la ciudad deseada)
 latitud = 6.2518
@@ -16,16 +10,9 @@ longitud = -75.5636
 
 # Construir la URL de la API con las coordenadas y la clave de la API
 url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitud}.34&lon={longitud}&appid={api_key}"
+response = requests.get(url).json()
 
-# Realizar la solicitud GET a la API
-res = requests.get(url)
-print("---------------------------")
-print("---------------------------")
 
-print(res)
 
-print("---------------------------")
-
-print("---------------------------")
 
 
