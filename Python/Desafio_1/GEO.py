d53e661ab65df589d63b509fffc12f22c426d068
Yen <yenifer.amayaos@gmail.com>
2024-01-03 21:33:48 -0500

    Geo_1

--- Python/Desafio_1/GEO.py
+++ Python/Desafio_1/GEO.py
@@ -1,2 +1,16 @@
 import datetime as dt
 import requests
+
+# Clave de la API desde las variables de entorno
+api_key = "bcc938b34848481d2843119f11e2b331"
+
+# Coordenadas de la ciudad
+city_name = 'Medellin'
+country_code =
+limit = 
+
+
+
+# Construir la URL de la API con las coordenadas y la clave de la API
+url = f"http://api.openweathermap.org/geo/1.0/direct?q={city name},{country code}&limit={limit}&appid={api_key}&units=metric"
+response = requests.get(url).json()
