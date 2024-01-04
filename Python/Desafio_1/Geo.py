fac2898d60343c27ea42731a0f75f583fbead8ad
Yen <yenifer.amayaos@gmail.com>
2024-01-03 20:13:40 -0500

    j

--- Python/Desafio_1/Geo.py
+++ Python/Desafio_1/Geo.py
@@ -12,6 +12,7 @@ longitud = -75.56
 url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitud}&lon={longitud}&appid={api_key}&units=metric"
 response = requests.get(url).json()
 
+##weather?lat={pipeline().parameters.lat}&lon={pipeline().parameters.lon}&appid=bcc938b34848481d2843119f11e2b331
 ##print(response)
 
 lon = response['coord']['lon']
